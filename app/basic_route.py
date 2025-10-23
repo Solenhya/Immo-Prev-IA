4337faf0f698356ef556fd6c44744ffe18085f63
Arnaud <arnaudrambourg@gmail.com>
2025-10-23 14:34:06 +0200

    feat: :sparkles: basic route creation

--- app/basic_route.py
+++ app/basic_route.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter , Request
 from fastapi.templating import Jinja2Templates
 router  = APIRouter()
 templates = Jinja2Templates(directory="templates")
-
+from . import prediction
 #Pour l'instant set comme sa mais a generer en fonction du modèle en production
-parameters = [{"label":"Surface bati","id":"surface_bati"},{"label":"Surface terrain","id":"surface_terrain"}]
+parameters = [{"label":"Surface du bati","id":"Surface_reelle_bati"},{"label":"Surface terrain","id":"Surface_terrain"},
+              {"label":"Nombre de pieces","id":"Nombre_pieces_principales"},{"label":"Type (Appartement ou Maison)","id":"Type_local"}]
 
 @router.get("/prediction_immo")
 async def prediction_page(request:Request):
@@ -14,5 +15,7 @@ async def prediction_page(request:Request):
 async def predict_immo(request:Request):
     form = await request.form()
     data = dict(form) 
-    print(data) #TODO rajouter la partie utilisation du modele
-    return {"price":42}
+    print(data)
+    prix = prediction.model_predict(data)
+    print(f"prix estimer:{prix}")
+    return {"price":prix[0]}
